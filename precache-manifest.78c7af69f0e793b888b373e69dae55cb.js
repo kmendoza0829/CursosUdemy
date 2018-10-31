f01bcf4ac92a18f95ef45a788f5a56642c58a6be
Kevin Mendoza Lopez <kevin.mendoza@serfinansa.com.co>
2018-10-31 08:22:34 -0500

    Updates

---
+++ precache-manifest.78c7af69f0e793b888b373e69dae55cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "839d26f97a1e3af3f4c4",
+    "url": "/CursosUdemy/static/js/runtime~main.839d26f9.js"
+  },
+  {
+    "revision": "42b8b668dae9fdb32e48",
+    "url": "/CursosUdemy/static/js/main.42b8b668.chunk.js"
+  },
+  {
+    "revision": "36e6af7bbf520bce8013",
+    "url": "/CursosUdemy/static/js/1.36e6af7b.chunk.js"
+  },
+  {
+    "revision": "42b8b668dae9fdb32e48",
+    "url": "/CursosUdemy/static/css/main.aa1123ea.chunk.css"
+  },
+  {
+    "revision": "7fc2cd6a08f78cef4ecbb4f5e6823cca",
+    "url": "/CursosUdemy/index.html"
+  }
+];
